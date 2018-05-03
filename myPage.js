2931fcf2d731ba7a57e96de0398beb4f3c6a730c
sirharis214 <36185908+sirharis214@users.noreply.github.com>
2018-05-03 08:15:00 -0700

    Add files via upload

---
+++ myPage.js
@@ -0,0 +1,13 @@
+$(document).scroll(function() {
+  var documentScrollTop = $(document).scrollTop();
+  if (documentScrollTop > $('#header').height()) {
+    $('#navbar').addClass('fixedattop');
+    var shiftContent = $('#navbar').height();
+    $('#content').css('margin-top', shiftContent + 'px');
+  }
+  else if ($('#navbar').hasClass('fixedattop')) {
+    $('#navbar').removeClass('fixedattop');
+    $('#content').css('margin-top', '0px');
+  }
+});
+
